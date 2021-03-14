075070652a0d131a9f41ba2c29bac55d91c29869
Radha <radhagupta.9bce@gmail.com>
2021-03-14 08:13:25 +0530

    changes_in_user_profile_query

--- Mysql_commands/Dashboard_query.sql
+++ Mysql_commands/Dashboard_query.sql
@@ -1,9 +1,11 @@
-  CREATE TABLE `user_details` (
+CREATE TABLE `user_profile` (
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `userId` int(11) NOT NULL,
     `loyalties_earned` int(10) NOT NULL,
     `current_corpus` int(10) NOT NULL,
     `required_corpus` int(10) NOT NULL,
+    `investment_profile_id` int(11) NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (userId) REFERENCES Users(id)
+    FOREIGN KEY (userId) REFERENCES Users(id),
+    FOREIGN KEY (investment_profile_id) REFERENCES investment_profile(id)
 );
